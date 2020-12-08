00708ecd46cd734aa8b5635241d47183ee4436be
iknowjason <jpo@pobox.com>
2020-12-08 11:43:33 -0600

    Update masscan_nmap3.py

--- masscan_nmap3.py
+++ masscan_nmap3.py
@@ -1,25 +1,23 @@
-### Step 1:  Edit your masscan command in the variable line below, correctly specifying your hosts and ports.  You don't need to change the '-oJ mscan.xml' line as this is required.
-masscan_command = "sudo masscan 192.168.7.0/24 --rate 20000 -p1-3000 -oG mscan.xml"
-#Example masscan_command = "sudo masscan 192.168.7.0/24 --rate 20000 -p1-65535 -oG mscan.xml"
-#Example masscan_command = "sudo masscan 192.168.7.0/24 -p1-65535 -oG mscan.xml"
-### Note:  Make sure your output file name is 'mscan.txt'
+### Step 1:  Run masscan with a JSON output file
+### Example:  sudo masscan 192.168.10.0/24 --rate 10000 -p1-65535 -oJ mscan.xml
+### Note:  Make sure you take note of your output file for next step
+### Important Note:  Tested on masscan 1.06 - There might be issues with older versions of masscan (i.e., 1.04) with JSON output
 
-### Step 2:  Run this script
-### python3 masscan_nmap3.py
+## Step 2:  Run this script that takes the output from Masscan JSON file and outputs a command you can use for nmap
+### Example:  python3 masscan_nmap3.py mscan.xml 
+### Note:  The script will create 'scans.txt' to be used by nmap; This file will allow you to run one nmap command per line, with each host and the ports listening that were enumerated by masscan
 
-import os
+## Step 3:  Run the command below which will loop through 'scans.txt' and run an nmap scan.  An output file will be created for each host:
+### Command: while IFS=' ' read -r host ports; do sudo nmap -n -vvv $host $ports -sV -sC -oN $host.txt;done < scans.txt
+
+import sys
 import json
 hosts = {}
 ports = "ports"
 
-### Run the masscan command
-print("[+] Running the masscan enumeration:  %s" % masscan_command)
-os.system(masscan_command)
-
-with open('mscan.xml') as json_file:
-
+with open('%s' % str(sys.argv[1])) as json_file:
     loaded_json = json.load(json_file)
-    for x in loaded_json:
+    for x in loaded_json: 
 
         ### Parse the port only if open (if you want TCP ports only - specify here)
         if x["ports"][0]["status"] == "open":
@@ -43,12 +41,10 @@ with open('mscan.xml') as json_file:
             else:
                 hosts[ip_addr][ports].append(port)
 
-
 # Create host and port scan text file
 text_file = open("scans.txt", 'w')
 
 hcount = 0
-cmds_list = []
 
 for h in hosts:
     port_str = "-p"
@@ -56,33 +52,16 @@ for h in hosts:
     # Write the host
     text_file.write("%s" % h)
     hcount+=1
-    tstring = h
-    tstring += str(':-p')
     for p in hosts[h]["ports"]:
+        print("    [+] Port: %s" % p)
         blah = str(p)
-        print("    [+] Port: %s" % blah)
         port_str += blah 
         port_str += str(",")
-        tstring += blah 
-        tstring += str(",")
     tmp_str = port_str[:-1]
     text_file.write(" %s\n" % tmp_str)
-
-    tstring = tstring[:-1]
-    cmds_list.append(tstring)
 print("[+] Created %d scan lines in text file: 'scans.txt'" % hcount)
-## save this file just for inspection
 text_file.close()
-
-### Loop through and run nmap command, running each scan against a single host with precise ports, and saving the file with IP address (i.e., <IP>.txt)
-# Declare the nmap base command
-nmap_base = "sudo nmap -n -vvv -sV -sC "
-for cmd in cmds_list:
-   #print("cmd: %s" % cmd)
-   tmp1 = cmd.split(':')
-   host = tmp1[0]
-   ports = tmp1[1]
-   #print("ports: %s" % ports)
-   full_nmap_cmd = nmap_base + host + " " + ports + " " + "-oN " + host + ".txt"
-   print("[+] Running nmap command: %s" % full_nmap_cmd)
-   os.system(full_nmap_cmd)
+### This is the command to run next:
+### while IFS=' ' read -r host ports; do echo "[+] Scanning $host"; sudo nmap -n -vvv $host $ports -sV -sC -oN $host.txt;done < scans.txt
+print("[+] Run the following command below to perform an nmap scan of each host with enumerated ports")
+print("Command:  while IFS=' ' read -r host ports; do sudo nmap -n -vvv $host $ports -sV -sC -oN $host.txt;done < scans.txt")

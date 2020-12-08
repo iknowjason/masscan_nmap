074028a638ef38e0eb44ce858a7422a7eda37cba
iknowjason <jpo@pobox.com>
2020-12-08 11:44:26 -0600

    Update masscan_nmap2.py

--- masscan_nmap2.py
+++ masscan_nmap2.py
@@ -1,27 +1,87 @@
-### Step 1:  Run masscan with a JSON output file
-### Example:  sudo masscan 192.168.10.0/24 --rate 10000 -p1-65535 -oJ mscan.xml
-### Note:  Make sure you take note of your output file for next step
-### Important Note:  Tested on masscan 1.06 - There might be issues with older versions of masscan (i.e., 1.04) with JSON output
+### Overview:  These steps will help you automate masscan and nmap together.  This script will do the following:
+# 1.  Automatically run masscan to enumerate all live hosts and listening ports, with output to an XML file 
+# 2.  Parse output XML file into live hosts (hosts.txt) and a normalized port string with all discovered ports 
+#     Note:  there might be extra port scans that run against hosts that don't have those listening ports (since the port list string is normalized across all live hosts.  If you want exact ports for each host, look at scripts 3 and 4)
+# 3.  Automatically run nmap command with parsed hosts and ports from masscan
+# 4.  Output nmap scan with -oA nmap_scan 
+# Important Note:  Tested on masscan 1.06 - Older versions (1.04, 1.05) will not work with JSON decoder 
 
-## Step 2:  Run this script that takes the output from Masscan JSON file and outputs a command you can use for nmap
-### Example:  python3 masscan_nmap2.py mscan.xml 
-### Note:  The script will create 'scans.txt' to be used by nmap; This file will allow you to run one nmap command per line, with each host and the ports listening that were enumerated by masscan
+# Author:  Jason Ostrom
 
-## Step 3:  Run the command below which will loop through 'scans.txt' and run an nmap scan.  An output file will be created for each host:
-### Command: while IFS=' ' read -r host ports; do sudo nmap -n -vvv $host $ports -sV -sC -oN $host.txt;done < scans.txt
+# Dependencies:
+# 1.  masscan 1.06
+# 2.  nmap
+# 3.  python3
 
+# Step 1: Edit the masscan base command variable (MASSCAN_CMD) in the line below to include your scope for target IP addresses and ports 
+# Optional:  Tune the rate (--rate).  Below 10,000 is used.  100 is the default, which is quite slow 
+MASSCAN_CMD = "sudo masscan 192.168.1.0/24 --rate 10000 -p1-1000 -oJ mscan.xml" 
+# Note:  Make sure you don't change the xml file output section (-oJ mscan.xml) as this is vital for the script to work 
+
+# Step 2: Edit the nmap base command variable (NMAP_CMD) in the line below to include your preferred nmap options
+# Note:  Don't touch HOSTS  and PORTS in the variable below ~ This is dynamically populated
+# The script dynamically creates a normalized port list string, so you don't have to worry about the port list and IP addresses
+NMAP_CMD = "sudo nmap -n -vvv -Pn -sV -sC HOSTS PORTS -oA nmap_scan"
+
+# Step 3:  Run this script
+# Example:  python3 masscan_nmap2.py
+###
+
+### import os
+import os
+
+### import sys
 import sys
+
+### import json
 import json
+
+## hosts dictionary
 hosts = {}
+
+## port string
 ports = "ports"
 
-with open('%s' % str(sys.argv[1])) as json_file:
+## host list
+host_list = []
+
+## port list
+port_list = []
+
+## initial port string
+port_list_str = "-p"
+
+## a counter for number of discovered hosts
+hcount = 0
+
+### Run the masscan command
+print("[+] Running the masscan enumeration:  %s" % MASSCAN_CMD)
+os.system(MASSCAN_CMD)
+
+### get filesize of 'mscan.xml'
+filesize = os.path.getsize("mscan.xml")
+
+### if the filesize is 0, exit (no data discovered) 
+if filesize == 0:
+    print ("[-] mscan.xml file is 0 bytes ~ (No discovered data)")
+    print ("    [-] Going to exit")
+    sys.exit()
+
+### open the mscan.xml file created from masscan
+with open("mscan.xml") as json_file:
+
+    ### load json
     loaded_json = json.load(json_file)
-    for x in loaded_json: 
+
+    ### loop through json
+    for x in loaded_json:
 
         ### Parse the port only if open (if you want TCP ports only - specify here)
         if x["ports"][0]["status"] == "open":
+
+            ### add the port to this data structure
             port = x["ports"][0]["port"]
+
             ip_addr = x["ip"]
             ### Add the IP address to dictionary if it doesn't already exist
             try:
@@ -40,28 +100,49 @@ with open('%s' % str(sys.argv[1])) as json_file:
                 pass
             else:
                 hosts[ip_addr][ports].append(port)
+                print("[+] Appending port: %s" % port)
+                port_list.append(port)
 
-# Create host and port scan text file
-text_file = open("scans.txt", 'w')
-
-hcount = 0
-
+### loop through and append the host to the hosts list 
 for h in hosts:
-    port_str = "-p"
-    print("[+] Host: %s" % h)
-    # Write the host
-    text_file.write("%s" % h)
+    print("[+] Appending host: %s" % h)
+    host_list.append(h)
     hcount+=1
-    for p in hosts[h]["ports"]:
-        print("    [+] Port: %s" % p)
-        blah = str(p)
-        port_str += blah 
-        port_str += str(",")
-    tmp_str = port_str[:-1]
-    text_file.write(" %s\n" % tmp_str)
-print("[+] Created %d scan lines in text file: 'scans.txt'" % hcount)
+
+## remove duplicate ports in list for combined ports
+port_list = list(dict.fromkeys(port_list))
+
+## Create a port list string
+for i in port_list: 
+    print("[+] Listing port in string: %s" % i)
+    portstring = str(i) 
+    port_list_str += portstring 
+    port_list_str += str(",")
+
+### modify the port list string to remove last ',' character
+tmp_str = port_list_str[:-1]
+
+# Output copy and paste port string
+print ("[+] Port list string: %s" % tmp_str)
+
+# Create hosts.txt file
+text_file = open("hosts.txt", 'w')
+for host in host_list:
+    text_file.write("%s\n" % host)
 text_file.close()
-### This is the command to run next:
-### while IFS=' ' read -r host ports; do echo "[+] Scanning $host"; sudo nmap -n -vvv $host $ports -sV -sC -oN $host.txt;done < scans.txt
-print("[+] Run the following command below to perform an nmap scan of each host with enumerated ports")
-print("Command:  while IFS=' ' read -r host ports; do sudo nmap -n -vvv $host $ports -sV -sC -oN $host.txt;done < scans.txt")
+### finished creating hosts.txt file
+
+## print informational
+print ("[+] Wrote %d hosts to file: hosts.txt" % hcount)
+
+### Build the new nmap command string, replacing HOST
+nmap_tmp_str = NMAP_CMD.replace("HOSTS", "-iL hosts.txt")
+
+### Build the new nmap command string, replacing PORTS 
+nmap_final_cmd = nmap_tmp_str.replace("PORTS", tmp_str)
+
+### Print final command before running it
+print ("[+] Runing this nmap command:  %s" % nmap_final_cmd)
+
+### Run final command
+os.system(nmap_final_cmd)
